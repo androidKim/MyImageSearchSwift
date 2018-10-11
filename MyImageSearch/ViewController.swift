80edd52e0b56e278960cef5d8be43d7786a92465
taejun <taejun@macbook-pro.local>
2018-10-11 16:04:02 +0900

    Initial Commit

---
+++ MyImageSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyImageSearch
+//
+//  Created by taejun on 2018. 10. 11..
+//  Copyright © 2018년 midas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
