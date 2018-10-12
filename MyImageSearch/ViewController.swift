30eb70b05e82635a809a01029ce70fbb90fed1ac
taejun <taejun@MacBook-Pro.local>
2018-10-12 18:19:10 +0900

    workingsample uitableview

--- MyImageSearch/ViewController.swift
+++ MyImageSearch/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    /********************** UITabelView Delegate Function **********************/
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        
+    }
+    //---------------------------------------------------------------------
+    //
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        let nCnt:Int = 10;
+        return nCnt;
+    }
+    //---------------------------------------------------------------------
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell") as! UITableViewCell
+        return cell
+    }
+    
+    /********************** Define **********************/
+    /********************** Member **********************/
+    let m_str_SearchText:NSString = ""
+    /********************** Controller **********************/
+    let m_TableView:UITableView = UITableView()
+    /********************** System function **********************/
+    //---------------------------------------------------------------------
+    //
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    //---------------------------------------------------------------------
+    //
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    /********************** User function **********************/
+    //---------------------------------------------------------------------
+    //
+    func initLayout()
+    {
+        self.m_TableView.dataSource = self
+        self.m_TableView.delegate = self
+        
+        self.getImageListProc()
+    }
+    //---------------------------------------------------------------------
+    //
+    func getImageListProc()
+    {
+        
+    }
 }
 
---
+++ MyImageSearch/listCell.swift
@@ -0,0 +1,21 @@
+//
+//  Cell.swift
+//  MyImageSearch
+//
+//  Created by taejun on 2018. 10. 12..
+//  Copyright © 2018년 midas. All rights reserved.
+//
+
+import UIKit
+
+class listCell: UITableViewCell
+{
+    @IBOutlet var cellView:UIView!
+    @IBOutlet var lb_Title:UILabel!
+    @IBOutlet var iv_Bg:UIImageView!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+}
